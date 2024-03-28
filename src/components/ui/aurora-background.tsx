4b39e815f1617451c8c66e1b04ce39e38c421eeb
João Vitor <jv.zaniolo@gmail.com>
2024-03-28 17:31:47 -0300

    remove unused deps

--- src/components/ui/aurora-background.tsx
+++ src/components/ui/aurora-background.tsx
@@ -1,10 +1,12 @@
-import { cn } from '@/utils';
+function classNames(...inputs: string[]) {
+  return inputs.join(' ');
+}
 
 export function AuroraBackground() {
   return (
     <div className="absolute inset-0 -z-10 overflow-hidden">
       <div
-        className={cn(
+        className={classNames(
           'pointer-events-none absolute -inset-[10px] opacity-50 blur-[10px] invert filter',
           '[--aurora:repeating-linear-gradient(100deg,theme(colors.blue.500)_10%,theme(colors.indigo.300)_15%,theme(colors.blue.300)_20%,theme(colors.violet.200)_25%,theme(colors.blue.400)_30%)]',
           '[--dark-gradient:repeating-linear-gradient(100deg,theme(colors.black)_0%,theme(colors.black)_7%,theme(colors.transparent)_10%,theme(colors.transparent)_12%,theme(colors.black)_16%)]',
